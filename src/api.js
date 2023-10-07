31cb3c3a90082e74b6d680ec4188c4d807a67313
moksh chadha <mokshchadha@mokshs-MacBook-Pro.local>
2023-10-07 14:59:56 +0530

    changed address

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const SERVER_URL = //FIXME: this is temporary hack for deployment over netlify t
   window.location.href.includes("localhost") ||
   window.location.href.includes("127.0.0.1")
     ? "http://localhost:3000"
-    : "https://filebin.net/gvj1652d71hktah7";
+    : "https://raw.githubusercontent.com/mokshchadha/protex_ai/main/server";
 
 console.log({ SERVER_URL });
 
